69c9ef06fc9c98253a99ed7081cb6e3213a478b4
Przemyslaw Cukierski <przemyslawcukierski98@gmail.com>
2021-03-19 08:00:55 +0100

    Added a model class representing song and corresponding controller

---
+++ Controllers/SongsController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SongsManagerMVC.Controllers
+{
+    public class SongsController : Controller
+    {
+        // GET: SongsController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: SongsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: SongsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SongsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SongsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SongsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SongsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SongsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/SongModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SongsManagerMVC.Models
+{
+    public class SongModel
+    {
+        public int Id { get; set; }
+        public string Album { get; set; }
+        public int Length { get; set; }
+        public string Artist { get; set; }
+        public string Description { get; set; }
+    }
+}
