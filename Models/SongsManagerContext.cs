3a5863377a0db4d8be849d69f3a16f1fa8105fc3
Przemyslaw Cukierski <przemyslawcukierski98@gmail.com>
2021-03-21 09:16:59 +0100

    Added connection to MSSQL database

---
+++ Models/SongsManagerContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SongsManagerMVC.Models
+{
+    public class SongsManagerContext : DbContext
+    {
+        public SongsManagerContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<SongModel> Songs { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SongsManagerMVC.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +26,8 @@ namespace SongsManagerMVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<SongsManagerContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("SongsManagerDatabase")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
