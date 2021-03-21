6c827976b022549e8f5e980be74f68b8fb867107
Przemyslaw Cukierski <przemyslawcukierski98@gmail.com>
2021-03-21 08:09:28 +0100

    A SongRepository and its corresponding ISongRepository interface have been created

---
+++ Repository/ISongRepository.cs
@@ -0,0 +1,17 @@
+﻿using SongsManagerMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SongsManagerMVC.Repository
+{
+    public interface ISongRepository
+    {
+        SongModel Get(int songId);
+        IQueryable<SongModel> GetAll();
+        void Add(SongModel song);
+        void Update(int id, SongModel song);
+        void Delete(int id);
+    }
+}
---
+++ Repository/SongRepository.cs
@@ -0,0 +1,36 @@
+﻿using SongsManagerMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SongsManagerMVC.Repository
+{
+    public class SongRepository : ISongRepository
+    {
+        public void Add(SongModel song)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SongModel Get(int songId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<SongModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, SongModel song)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
